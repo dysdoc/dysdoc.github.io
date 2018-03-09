d27b790011b2f2e1ea76ac86ccb34b31aa81f7e3
ojcchar <ojcch1@gmail.com>
2018-03-09 00:27:05 -0600

    fixing the google maps api call

--- js/scripts.js
+++ js/scripts.js
@@ -52,62 +52,63 @@ jQuery(function ($) {
         });
     }());
 
-    // --------------------------------------------------------------------
-    // Google Map
-    // --------------------------------------------------------------------
-
-    (function () {
-        if ($('#googleMap').length > 0) {
-
-            //set your google maps parameters
-            var $latitude  = 40.427994, //If you unable to find latitude and longitude of your address. Please visit http://www.latlong.net/convert-address-to-lat-long.html you can easily generate.
-                $longitude = -3.667810,
-                $map_zoom  = 18;
-            /* ZOOM SETTING */
-
-            //google map custom marker icon
-            var $marker_url = 'img/google-map-marker.png';
-
-            //we define here the style of the map
-            var style = [];
-            /*var style = [{
-                "stylers" : [{
-                    "hue" : "#000"
-                }, {
-                    "saturation" : 100
-                }, {
-                    "gamma" : 1.15
-                }, {
-                    "lightness" : 5
-                }]
-            }];*/
-
-            //set google map options
-            var map_options = {
-                center            : new google.maps.LatLng($latitude, $longitude),
-                zoom              : $map_zoom,
-                panControl        : false,
-                zoomControl       : false,
-                mapTypeControl    : false,
-                streetViewControl : false,
-                mapTypeId         : google.maps.MapTypeId.ROADMAP,
-                scrollwheel       : false,
-                styles            : style,
-            }
-            //initialize the map
-            var map = new google.maps.Map(document.getElementById('googleMap'), map_options);
-            //add a custom marker to the map
-            var marker = new google.maps.Marker({
-                position : new google.maps.LatLng($latitude, $longitude),
-                map      : map,
-                visible  : true
-                //icon     : $marker_url
-            });
-        }
-    }());
 
 }); // JQuery end
 
+
+// --------------------------------------------------------------------
+// Google Map
+// --------------------------------------------------------------------
+function initMap() {
+	if ($('#googleMap').length > 0) {
+
+		//set your google maps parameters
+		var $latitude  = 40.427994, //If you unable to find latitude and longitude of your address. Please visit http://www.latlong.net/convert-address-to-lat-long.html you can easily generate.
+			$longitude = -3.667810,
+			$map_zoom  = 18;
+		/* ZOOM SETTING */
+
+		//google map custom marker icon
+		var $marker_url = 'img/google-map-marker.png';
+
+		//we define here the style of the map
+		var style = [];
+		/*var style = [{
+			"stylers" : [{
+				"hue" : "#000"
+			}, {
+				"saturation" : 100
+			}, {
+				"gamma" : 1.15
+			}, {
+				"lightness" : 5
+			}]
+		}];*/
+
+		//set google map options
+		var map_options = {
+			center            : new google.maps.LatLng($latitude, $longitude),
+			zoom              : $map_zoom,
+			panControl        : false,
+			zoomControl       : false,
+			mapTypeControl    : false,
+			streetViewControl : false,
+			mapTypeId         : google.maps.MapTypeId.ROADMAP,
+			scrollwheel       : false,
+			styles            : style,
+		}
+		//initialize the map
+		var map = new google.maps.Map(document.getElementById('googleMap'), map_options);
+		//add a custom marker to the map
+		var marker = new google.maps.Marker({
+			position : new google.maps.LatLng($latitude, $longitude),
+			map      : map,
+			visible  : true
+			//icon     : $marker_url
+		});
+	}
+}
+	
 (function($, viewport){
     $(document).ready(function() {
 	
